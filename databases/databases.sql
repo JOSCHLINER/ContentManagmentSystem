2aec41e74aca1e1312b0a5a294594b5ae4bef7ea
joschliner <joschliner@users.noreply.github.com>
2024-01-11 22:39:12 +0100

    create database user  user

--- databases/databases.sql
+++ databases/databases.sql
@@ -1,10 +1,27 @@
-CREATE DATABASE test;
-USE test;
+-- creating the database and the tables alongside it
+CREATE DATABASE IF NOT EXISTS contentmanagment;
+USE contentmanagment;
 
-CREATE TABLE users(
-    id INT PRIMARY KEY AUTO_INCREMENT, 
-    username VARCHAR(100) NOT NULL UNIQUE
+CREATE TABLE IF NOT EXISTS users(
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(16) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    privileges ENUM('a', 'u', 'm', 'n') DEFAULT 'u' NOT NULL
 );
 
-INSERT INTO users(id, username)
-VALUES(1, 'testuser');
+CREATE TABLE IF NOT EXISTS articles(
+    article_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_id INT NOT NULL, 
+    content TEXT NOT NULL,
+    created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    
+    FOREIGN KEY (author_id) REFERENCES users(user_id)
+);
+
+-- Creating the user for the content managment system to use
+CREATE USER IF NOT EXISTS 'cmswebbpage'@'localhost'
+IDENTIFIED WITH authentication_plugin BY 'secure_password';
+
+GRANT INSERT, UPDATE, DELETE, SELECT ON contentmanagment.* TO 'cmswebbpage'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
