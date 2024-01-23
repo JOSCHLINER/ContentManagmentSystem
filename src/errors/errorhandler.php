427ba0be7735504a07052c9e1d82366947f2f41b
joschliner <joschliner@users.noreply.github.com>
2024-01-23 23:11:46 +0100

    added ErrorHandler class

---
+++ src/errors/errorhandler.php
@@ -0,0 +1,17 @@
+<?php
+
+class ErrorHandler
+{
+
+    public function __construct()
+    {
+    }
+
+    public static function displayFatalServerError() {
+        
+    }
+
+    public static function displayUserInputError() {
+        
+    }
+}
