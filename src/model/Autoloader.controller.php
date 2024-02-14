14c3be556595d56f8d28b59b0fe6c0566ad96a14
joschliner <joschliner@users.noreply.github.com>
2024-02-14 23:34:54 +0100

    created Autoloading class #4

---
+++ src/model/Autoloader.controller.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * Autoloader Class
+ * 
+ * This Autoloader follows the PSR-4 standards for autoloading classes.
+ * The base directory is the `src/` directory.
+ * 
+ * Example:
+ * `Model\Database` => `src/model/Database.model.php`
+ * 
+ * The Autoloader is initialized as follows:
+ * ```php
+ * require __DIR__ . '/Autoloader.controller.php';
+ * Autoloader::register();
+ * ```
+ * @see https://www.php-fig.org/psr/psr-4/
+ */
+class Autoloader
+{
+
+    /**
+     * The function registers the Autoloader.
+     * It enables automatic class loading based on PSR-4 standards.
+     * The base directory for the autoloader is `src/`.
+     */
+    public static function register(): void
+    {
+        spl_autoload_register([__CLASS__, 'loader']);
+    }
+
+    private static function loader(string $className): bool
+    {
+        $classParts = explode('\\', $className);
+
+        $filename = end($classParts) . '.' . lcfirst($classParts[0]) . '.php';
+        $directoryPath = implode('/', array_map('lcfirst', array_slice($classParts, 1, -1)));
+
+        $fullPath = __DIR__ . '/../' . $directoryPath . '/' . $filename;
+
+        // check if the file exists
+        if (file_exists($fullPath)) {
+            require $fullPath;
+            return true;
+        }
+
+        return false;
+    }
+}
