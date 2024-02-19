0dfee632af4788628e672291ed23c025e0c29542
joschliner <joschliner@users.noreply.github.com>
2024-02-19 16:35:56 +0100

    adjusted class to use the object in the session

--- src/controller/users/Authenticate.controller.php
+++ src/controller/users/Authenticate.controller.php
@@ -2,30 +2,20 @@
 namespace Controller\Users;
 
 
-use Controller\Users\Users;
-
-class Authenticate extends Users
+class Authenticate
 {
-    public static function privilegesAreSet()
+    public static function isLoggedIn()
     {
-        if (!isset($_SESSION['userPrivileges'])) {
-            $_SESSION['userPrivileges'] = 'n';
-        }  
-        return true;
+        return isset($_SESSION['user']);
     } 
-
-    public static function isLoggedIn(): bool
-    {
-        return self::privilegesAreSet() and $_SESSION['userPrivileges'] != 'n';
-    }
 
     public static function isModeratorOrGreater(): bool
     {
-        return self::privilegesAreSet() and ($_SESSION['userPrivileges'] == 'm' or self::isAdministrator());
+        return self::isLoggedIn() and (unserialize($_SESSION['user'])->authorization == 'm' or self::isAdministrator());
     }
 
     public static function isAdministrator(): bool
     {
-        return self::privilegesAreSet() and $_SESSION['userPrivileges'] == 'a';
+        return self::isLoggedIn() and unserialize($_SESSION['user'])->authorization == 'u'; # should be changed to a for administrator later
     }
 }
