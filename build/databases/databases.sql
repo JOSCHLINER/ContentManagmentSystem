95ca3133b600e836d0504c73975169e4d0343cbf
joschliner <joschliner@users.noreply.github.com>
2024-02-19 15:11:22 +0100

    changed size of maximum username

--- build/databases/databases.sql
+++ build/databases/databases.sql
@@ -4,7 +4,7 @@ USE contentmanagment;
 
 CREATE TABLE IF NOT EXISTS users(
     user_id INT PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(16) UNIQUE NOT NULL,
+    username VARCHAR(64) UNIQUE NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     privileges ENUM('a', 'u', 'm', 'n') DEFAULT 'u' NOT NULL
