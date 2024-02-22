37b4ab99e5ad05f97b9ea0e0d9d62c3516553ffa
joschliner <joschliner@users.noreply.github.com>
2024-02-22 23:52:00 +0100

    added login register functionality to pages

--- src/view/public/login.php
+++ src/view/public/login.php
@@ -1,39 +1,74 @@
 <?
 // initialize necessary services
 require __DIR__ . '/../../model/Includes.model.php';
-Model\Includes::initialize();
+Model\Includes::initialize(false);
 
-
-use Controller\Error\HTTPResponse;
+use View\Templates\Pages;
 use Controller\Users\Login;
-use Controller\Settings\Settings;
 
-if ($_SERVER['REQUEST_METHOD'] != 'POST') {
-    HTTPResponse::displayForbidden();
+/**
+ * Class for the login page.
+ */
+class LoginPage extends Pages
+{
+
+    protected string $errorPath = '/login.php';
+    public function __construct()
+    {
+        $this->pageTitle = 'Login';
+
+        parent::__construct();
+    }
+
+    protected function pageContent()
+    {
+?>
+
+        <form method="Post">
+            <div class="form-group">
+                <label for="exampleInputEmail1">Username</label>
+                <input type="text" class="form-control" id="exampleInputEmail1" name="username" aria-describedby="emailHelp" placeholder="Enter username">
+            </div>
+
+            <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" name="password" placeholder="Password">
+            </div>
+
+            <input type="submit" class="btn btn-primary" value="Login">
+        </form>
+
+        <div class="d-block">
+            <p>Don't have an account? <a href="register.php" class="btn btn-link">Register</a></p>
+        </div>
+
+<?php
+    }
+
+    public function handlePostRequest(array &$Request)
+    {
+
+        // try to login user
+        $handler = new login();
+        $user = $handler->login($_POST);
+
+        // store user data in session
+        $_SESSION['user'] = serialize($user);
+
+        // redirect user to starting page
+        header('Location: index.php?err=Logged in successfully as ' . $user->username . '.&type=success'); # should display to user that they are successfully logged in
+    }
 }
 
-// load settings into database
-$settings = new Settings();
-$settings->loadDatabase();
+// rendering page
+$page = new LoginPage();
 
-// try to login user
 try {
-    $handler = new login();
-    $user = $handler->login($_POST);
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $page->handlePostRequest($_POST);
+    } else {
+        $page->renderOnlyContent();
+    }
 } catch (Error $error) {
-    // catch any errors created from users input
-
-    // redirect user to the homepage and show the error created
-    header('Location: index.php?err=' . $error->getMessage() . '&type=error');
-    exit($error->getMessage());
-} catch (Exception $error) {
-    // catch any internal errors
-
-    exit($error->getMessage());
+    $page->errorRedirect('An error occurred when trying to log in!', $error->getMessage());
 }
-
-// store user data in session
-$_SESSION['user'] = serialize($user);
-
-// redirect user to starting page
-header('Location: index.php?err=Logged in successfully as ' . $user->username . '.&type=success'); # should display to user that they are successfully logged in
--- src/view/public/register.php
+++ src/view/public/register.php
@@ -1,36 +1,84 @@
 <?
 // initialize necessary services
 require __DIR__ . '/../../model/Includes.model.php';
-Model\Includes::initialize();
+Model\Includes::initialize(false);
 
 
-use Controller\Error\HTTPResponse;
 use Controller\Users\Register;
-use Controller\Settings\Settings;
+use View\Templates\Pages;
 
-if ($_SERVER['REQUEST_METHOD'] != 'POST') {
-    HTTPResponse::displayForbidden();
+/**
+ * Class for the register page
+ */
+class RegisterPage extends Pages
+{
+
+    protected string $errorPath = '/register.php';
+    public function __construct()
+    {
+        $this->pageTitle = 'Create Page';
+
+        parent::__construct();
+    }
+
+    protected function pageContent()
+    {
+?>
+
+        <form method="Post">
+            <div class="form-group">
+                <label for="usernameInput">Username</label>
+                <input type="text" class="form-control" id="usernameInput" name="username" aria-describedby="usernameHelp" placeholder="Enter username" maxlength="64" required>
+            </div>
+
+            <div class="form-group">
+                <label for="emailInput">Email address</label>
+                <input type="email" class="form-control" id="emailInput" name="email" aria-describedby="emailHelp" placeholder="Enter email" required>
+            </div>
+
+            <div class="form-group">
+                <label for="passwordInput">Password</label>
+                <input type="password" class="form-control" id="passwordInput" name="password" placeholder="Password" required>
+            </div>
+
+            <div class="form-check">
+                <input type="checkbox" class="form-check-input" id="tosCheckbox" required>
+                <label class="form-check-label" for="tosCheckbox">I agree to the <a href="#">Terms and Services</a></label>
+            </div>
+
+            <input type="submit" class="btn btn-primary" value="Register">
+        </form>
+
+        <div class="d-block">
+            <p>Already have an account? <a href="login.php" class="btn btn-link">Login</a></p>
+        </div>
+
+
+<?php
+    }
+
+    public function handlePostRequest(array &$Request)
+    {
+        // try to create user
+        $handler = new Register();
+        $username = $handler->register($_POST);
+
+
+        // redirecting user to the login page
+        // easier to let user login that it is to login the user directly
+        header("Location: register.php?err=$username created Successfully&type=success");
+    }
 }
 
-// load settings into database
-$settings = new Settings();
-$settings->loadDatabase();
+// rendering page
+$page = new RegisterPage();
 
-// try to create user
 try {
-    $handler = new Register();
-    $username = $handler->register($_POST);
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $page->handlePostRequest($_POST);
+    } else {
+        $page->renderOnlyContent();
+    }
 } catch (Error $error) {
-    // catch any errors created from users input
-
-    header('Location: index.php?err=' . $error->getMessage() . '&type=warning');
-    exit($error->getMessage());
-} catch (Exception $error) {
-    // catch any internal errors
-
-    exit($error->getMessage());
+    $page->errorRedirect('An error occurred when trying to create the user!', $error->getMessage());
 }
-
-// redirecting user to the login page
-// easier to let user login that it is to login the user directly
-header('Location: index.php?err=User created Successfully&type=success');
