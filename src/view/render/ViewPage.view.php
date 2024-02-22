18f46794e02393f5db222ba4cc4a187b1b182879
joschliner <joschliner@users.noreply.github.com>
2024-02-22 23:47:34 +0100

    added class to render pages markdown

---
+++ src/view/render/ViewPage.view.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace View\Render;
+
+use Controller\Pages\PagesHandler;
+use Libraries\Parsedown;
+use Model\Page;
+use Model\Includes;
+
+/**
+ * Class to creating pages rendering sites.
+ */
+class ViewPage {
+
+     private int $pageId;
+     private Page $page;
+     private Parsedown $parser;
+     public function __construct(int $pageId) {
+          $this->pageId = $pageId;
+
+          $handler = new PagesHandler();
+          $this->page = $handler->getPage($this->pageId);
+
+          $this->parser = Includes::getSecureParsedown();
+     }
+
+     public function renderMarkdownMainContent() {
+          return $this->parser->text($this->page->pageContent);
+     }
+
+     public function renderMarkdownSummary() {
+          return $this->parser->text($this->page->pageSummary);
+     }
+
+     public function pageTitle(): string {
+          return $this->page->pageTitle;
+     }
+
+     public function pageAuthor(): string {
+          return $this->page->pageAuthor;
+     }
+
+     public function pageCreationDate(): string {
+          return $this->page->creationDate;
+     }
+
+}
