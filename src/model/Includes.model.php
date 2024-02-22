ab1eb3e4eb63d1c88e11452a7213f8f4e6704e17
joschliner <joschliner@users.noreply.github.com>
2024-02-22 23:54:51 +0100

    added functions needed in many pages into class

--- src/model/Includes.model.php
+++ src/model/Includes.model.php
@@ -3,6 +3,8 @@
 namespace Model;
 
 use Libraries\Parsedown;
+use Controller\Settings\Settings;
+use Controller\Users\Authenticate;
 
 /**
  * Class for view pages. Initializes important services.
@@ -11,31 +13,50 @@ use Libraries\Parsedown;
  * - Autoloader
  * - Session
  */
-class Includes {
+class Includes
+{
 
-    public static function initialize() {
+    public static function initialize(bool $loginRequired = true)
+    {
         self::initializeAutoloader();
         self::checkSessionStatus();
 
+        if ($loginRequired) {
+            self::checkLoginStatus();
+        }
+
+        //load Settings into database
+        $settings = new Settings();
+        $settings->loadDatabase();
     }
 
-    private static function initializeAutoloader() {
+    private static function initializeAutoloader()
+    {
         // load and register the Autoloader
         include __DIR__ . '/Autoloader.model.php';
         Autoloader::register();
     }
 
-    private static function checkSessionStatus() {
+    private static function checkLoginStatus()
+    {
+        if (!Authenticate::isLoggedIn()) {
+            http_response_code(403);
+            header('Location: signup.php');
+        }
+    }
+
+    private static function checkSessionStatus()
+    {
         if (session_status() == PHP_SESSION_NONE) {
             session_start();
         }
     }
 
-    public static function getSecureParsedown(): Parsedown {
+    public static function getSecureParsedown(): Parsedown
+    {
         $parser = new Parsedown();
         $parser->setSafeMode(true);
 
         return $parser;
     }
-
 }
