6b6441f4d17f2ff35f4d08d96853ff87ed07a77d
joschliner <joschliner@users.noreply.github.com>
2024-02-23 14:40:39 +0100

    added edit button

--- src/view/public/view.php
+++ src/view/public/view.php
@@ -28,7 +28,17 @@ class View extends Pages
 ?>
 
         <div class="view_header">
-            <h1> <?= $this->pageRender->pageTitle() ?></h1>
+            <div class="d-flex justify-content-between">
+                <h1> <?= $this->pageRender->pageTitle() ?></h1>
+                <a href="/edit/<?= $this->pageRender->pageId() ?>">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                        <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
+                    </svg>
+                    <span style="font-size:small">Edit</span>
+                </a>
+            </div>
+
             <div class="d-flex justify-content-center">
                 <div class="d-flex justify-content-between" style="width: 98%">
                     <span>written by <u> <?= $this->pageRender->pageAuthor() ?> </u> </span>
@@ -61,6 +71,7 @@ try {
     $page = new View($pageId);
 } catch (Error $error) {
     header('Location: /index.php?err=The page you tried to view does not exist!&msg=' . $error->getMessage());
+    exit();
 }
 
 
