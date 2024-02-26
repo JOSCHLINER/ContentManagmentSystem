a2a83ed109e807fca8e366ee88971befcd33fbab
joschliner <joschliner@users.noreply.github.com>
2024-02-26 10:05:48 +0100

    added restricted mode

--- src/view/public/create.php
+++ src/view/public/create.php
@@ -26,7 +26,7 @@ class Create extends Pages
             Markdown is enabled.
         </div>
 
-        <form class="needs-validation"method="Post">
+        <form class="needs-validation" method="Post">
             <div class="mb-3">
                 <label for="title" class="form-label">Title</label>
                 <input type="text" class="form-control" id="title" name="title" aria-describedby="Title" maxlength="100" required>
@@ -45,7 +45,13 @@ class Create extends Pages
             </div>
 
             <div class="mb-3 form-check">
-                <input type="checkbox" class="form-check-input" id="tos" name="" required>
+                <input type="checkbox" class="form-check-input" id="restricted_mode" name="restricted_mode">
+                <label class="form-check-label" for="restricted_mode">Set page in restricted mode</label>
+                <small id="restricted_mode" class="form-text text-muted d-block">Restricted Mode only allows you the author to edit the page.</small>
+            </div>
+
+            <div class="mb-3 form-check">
+                <input type="checkbox" class="form-check-input" id="tos" name="tos" required>
                 <label class="form-check-label" for="tos">Consent to the <a href="#">Terms and Conditions</a>.</label>
             </div>
             <button type="submit" class="btn btn-primary">Create Page</button>
@@ -65,8 +71,9 @@ class Create extends Pages
         $handler = new PagesHandler();
         try {
             // trying to create the page
+            $pageRestrictionMode = (isset($Request['restricted_mode'])) ? (($Request['restricted_mode'] == 'on') ? true : false) : false;
 
-            $pageId = $handler->createPage($Request['content'], $Request['summary'], $Request['title']);
+            $pageId = $handler->createPage($Request['content'], $Request['summary'], $Request['title'], $pageRestrictionMode);
             if (is_null($pageId)) {
                 throw new Error();
             }
@@ -100,6 +107,9 @@ class Create extends Pages
             return false;
         } elseif (strlen($Request['content']) < 100) {
             throw new Error('Please give the page some more content. At least 100 characters are needed!');
+            return false;
+        } elseif (!isset($Request['tos']) or $Request['tos'] == 'off') {
+            throw new Error('You have to agree to our terms of services if you want to create a page!');
             return false;
         }
 
